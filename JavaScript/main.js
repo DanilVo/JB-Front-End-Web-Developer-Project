ee5aa3a16f90a0606a8642d224ac8a0d6b1c49d0
DanilV <dani736@gmail.com>
2023-07-09 22:11:02 +0300

    finished search, started working on user favorite coins

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,36 +1,81 @@
-window.addEventListener('load', sortData());
+const LIST_OF_COINS_LS_ID = "coin";
+const SELECTED_COINS_LS_ID = "userCoins";
+const URL_API =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
+window.addEventListener(
+  "load",
+  drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID))
+);
 
+// async function getDataFromApi() {
+//   try {
+//     const response = await fetch(URL_API);
+//     const data = await response.json();
+//     setToLocalStorage(data, LIST_OF_COINS_LS_ID);
+//     drawData(data);
+//   } catch (err) {
+//     drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID));
+//     // console.log(err);
+//   }
+// }
 
-async function getDataFromApi() {
-  const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
-  const data = await response.json();
-  setToLocalStorage(data);
-}
-
-function sortData() {
-  const cards = document.querySelector('.cards');
-  const data = getFromLocalStorage() || [];
-  let html = '';
-  for (let i = 0; i < 1; i++) {
+function drawData(data) {
+  const cards = document.querySelector(".row");
+  let html = "";
+  for (let i = 0; i < data.length; i++) {
     html += `
-    <div class="card">
-    <h5 class="card-header">Featured</h5>
-    <div class="card-body">
-      <h5 class="card-title">${data}</h5>
-      <p class="card-text">${data[i].symbol}</p>
-      <a href="#" class="btn btn-primary">${data[i].name}</a>
-    </div>
-  </div>`;
+    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
+      <div class="card" id="${data[i].id}">
+        <h5 class="card-header">${data[i].symbol.toUpperCase()}</h5>
+        <div class="card-body">
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+          </div>
+            <h5 class="card-title">${data[i].name}</h5>
+            <p class="card-text"></p>
+            <a href="javascript:void(0)" class="btn btn-primary">Show Info</a>
+        </div>
+      </div>
+    </div>`;
   }
-  cards.innerHTML = html;
+  if (data.length === 0) {
+    cards.innerHTML = "Please enter valid name of coin";
+  } else {
+    cards.innerHTML = html;
+  }
 }
 
-function setToLocalStorage(data) {
-  localStorage.removeItem('coins');
-  localStorage.setItem('coin', JSON.stringify(data));
+function setToLocalStorage(data, id) {
+  localStorage.removeItem(id);
+  localStorage.setItem(id, JSON.stringify(data));
 }
 
-function getFromLocalStorage() {
-  const coins = localStorage.getItem('coin');
-  return coins;
+function getFromLocalStorage(id) {
+  const coins = localStorage.getItem(id);
+  return JSON.parse(coins);
 }
+
+const input = document.querySelector(".form-control");
+input.addEventListener("keyup", () => displaySearchCoins(input.value));
+
+function displaySearchCoins(coins) {
+  const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
+  const listOfCoins = coinsList.filter((coin) => {
+    return coin.name.toLowerCase().includes(coins);
+  });
+  drawData(listOfCoins);
+}
+
+let favoriteCoinsArr = []
+
+const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+checkBoxes.forEach((key) =>
+  key.addEventListener("click", function () {
+    const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
+    const checkedCoin = this.parentElement.parentElement.parentElement.id
+    const getCoinFromLS = coinsList.find((item)=> item.id == checkedCoin)
+    // favoriteCoinsArr.push(getCoinFromLS);
+    console.log(favoriteCoinsArr);
+    console.log(this.checked);
+  })
+);
