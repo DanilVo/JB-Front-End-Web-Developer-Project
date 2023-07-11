b0711044ad6689a47f3314249e13aa32431f94bd
DanilV <dani736@gmail.com>
2023-07-11 22:48:49 +0300

    fix checkbox modal

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,22 +2,17 @@ const LIST_OF_COINS_LS_ID = "coin";
 const SELECTED_COINS_LS_ID = "userCoins";
 const URL_API =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
-window.addEventListener(
-  "load",
-  getDataFromApi()
-);     
+window.addEventListener("load", getDataFromApi());
 
 async function getDataFromApi() {
   try {
     const response = await fetch(URL_API);
     const data = await response.json();
-    console.log(response);
     setToLocalStorage(data, LIST_OF_COINS_LS_ID);
-    drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID));
-    console.log('here');
+    drawData(data);
   } catch (err) {
     drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID));
-    console.log(err); 
+    console.log(err);
   }
 }
 
@@ -30,8 +25,8 @@ function drawData(data) {
       <div class="card" id="${data[i].id}">
         <h5 class="card-header">${data[i].symbol.toUpperCase()}</h5>
         <div class="card-body">
-          <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+          <div class="form-check-reverse form-switch">
+            <input class="form-check-input ${data[i].symbol}" type="checkbox" role="switch" id="flexSwitchCheckDefault">
           </div>
             <h5 class="card-title">${data[i].name}</h5>
             <p class="card-text"></p>
@@ -45,6 +40,37 @@ function drawData(data) {
   } else {
     cards.innerHTML = html;
   }
+  const checkBoxes = document.querySelectorAll("#flexSwitchCheckDefault");
+  const favoriteCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
+  if (favoriteCoins) {
+    checkBoxes.forEach(box=>{      
+      const iterator = favoriteCoins.values()
+      for (const value of iterator) {
+        if (box.classList.contains(value.symbol)) {
+          box.setAttribute("checked", "checked");
+        }
+      }
+    })
+  }
+
+  checkBoxes.forEach((key) =>
+    key.addEventListener("click", function () {
+      const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
+      const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || []
+      const checkedCoin = this.parentElement.parentElement.parentElement.id;
+      const findCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
+      if (this.checked) {
+        setToLocalStorage([...userCoins,findCoinFromLS], SELECTED_COINS_LS_ID);
+        countFavoriteCoins();
+      } else {
+        const indexOfRemovedCoin = userCoins.findIndex(
+          (item) => item.id == checkedCoin
+        );
+        userCoins.splice(indexOfRemovedCoin, 1);
+        setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
+      }
+    })
+  );
 }
 
 function setToLocalStorage(data, id) {
@@ -58,7 +84,7 @@ function getFromLocalStorage(id) {
 }
 
 const input = document.querySelector(".form-control");
-input.addEventListener("keyup", () => displaySearchCoins(input.value));
+input.addEventListener("input", () => displaySearchCoins(input.value));
 
 function displaySearchCoins(coins) {
   const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
@@ -68,26 +94,19 @@ function displaySearchCoins(coins) {
   drawData(listOfCoins);
 }
 
-let favoriteCoinsArr = [];
-const checkBoxes = document.querySelectorAll(".form-check-input");
-// console.log(checkBoxes);
-checkBoxes.forEach((key) =>
-  key.addEventListener("click", function () {
-    const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
-    const checkedCoin = this.parentElement.parentElement.parentElement.id;
-    const getCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
-    console.log(this.checked);
-    if (this.checked) {
-      favoriteCoinsArr.push(getCoinFromLS);
-      setToLocalStorage(favoriteCoinsArr, SELECTED_COINS_LS_ID);
-    } else {
-      const indexOfRemovedCoin = favoriteCoinsArr.findIndex(
-        (item) => item.id == checkedCoin
-      );
-      favoriteCoinsArr.splice(indexOfRemovedCoin, 1);
-      setToLocalStorage(favoriteCoinsArr, SELECTED_COINS_LS_ID);
-    }
-    // console.log(getCoinFromLS);
-    // console.log(favoriteCoinsArr);
+function countFavoriteCoins() {
+  const checkBoxes = document.querySelectorAll("#flexSwitchCheckDefault");
+  const favoriteCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
+  checkBoxes.forEach(box=>{
+    box.addEventListener('input',function(){
+      
+      if (favoriteCoins.length > 5) {
+        console.log(favoriteCoins.length);
+        box.setAttribute("data-bs-toggle", "modal");
+        box.setAttribute("data-bs-target", "#exampleModal");
+        this.checked = false
+      }
+
+    })
   })
-);
+}
