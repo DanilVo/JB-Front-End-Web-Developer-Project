e5c523e927aea43cb0f6b073201b5a9604a2c6ed
DanilV <dani736@gmail.com>
2023-07-20 15:44:14 +0300

    finish replace coin function

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,12 @@
-const LIST_OF_COINS_LS_ID = 'coin';
-const SELECTED_COINS_LS_ID = 'userCoins';
-const PRICE_OF_COINS = 'coinsPrices';
+const LIST_OF_COINS_LS_ID = "coin";
+const SELECTED_COINS_LS_ID = "userCoins";
+const PRICE_OF_COINS = "coinsPrices";
 const URL_API =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1';
-document
-  .getElementById('home-page')
-  .addEventListener(
-    'click',
-    drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID))
-  );
-window.addEventListener('load', getDataFromApi());
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
+
+document.getElementById("home-page").addEventListener("click", getDataFromApi);
+
+window.addEventListener("load", getDataFromApi());
 
 async function getDataFromApi() {
   try {
@@ -22,50 +19,82 @@ async function getDataFromApi() {
       drawData(getFromLocalStorage(LIST_OF_COINS_LS_ID));
     } else {
       cards.innerHTML =
-        '<h1 style="background-color: yellow">some thing went wrong</h1>';
+        '<h1 style="background-color: yellow">Some thing went wrong:(</h1>';
     }
     console.log(`${err}, please try again later.`);
   }
 }
 
 function onToggleChange(element) {
-  //add element as argument to follow input
   const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
-  const userCheckBoxes = document.querySelectorAll('.user-selected');
-  console.log(userCoins.length);
+  const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
+  const userCheckBoxes = document.querySelectorAll(".user-selected");
   if (userCoins.length === 5) {
     userCheckBoxes.forEach((item) => {
-      item.setAttribute('data-bs-toggle', 'modal');
-      item.setAttribute('data-bs-target', '#exampleModal');
+      item.setAttribute("data-bs-toggle", "modal");
+      item.setAttribute("data-bs-target", "#exampleModal");
     });
   } else if (userCoins.length <= 4) {
     userCheckBoxes.forEach((item) => {
-      if (item.hasAttribute('data-bs-toggle')) {
+      if (item.hasAttribute("data-bs-toggle")) {
         console.log(userCoins.length);
-        // item.checked = false; // not suppose to be here
-        item.removeAttribute('data-bs-toggle');
-        item.removeAttribute('data-bs-target');
+        item.removeAttribute("data-bs-toggle");
+        item.removeAttribute("data-bs-target");
       }
     });
   }
   if (userCoins.length > 5) {
     element.checked = false;
-    // this.disabled = true;
     userCoins.splice(userCoins.length - 1, 1);
     setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
+    const element1 = element.parentElement.parentElement.parentElement.id;
+    const findCoinFromLS = coinsList.find((item) => item.id === element1);
+    
+    clearCoin(findCoinFromLS);
   }
 }
 
-function unchoseCoin() {
-  
+function clearCoin(element) {
+    const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
+  const cards = document.querySelector("#selectedCoins");
+  cards.innerHTML = htmlTemplate(userCoins);
+  placeCoinsInModal(element);
 }
 
-function drawData(data) {
-  const cards = document.querySelector('.row');
-  let html = '';
+function placeCoinsInModal(element) {
+    console.log(element);
+    const cards = document.querySelectorAll(
+      "#selectedCoins > #responsiveLayout"
+    );
+  cards.forEach((card) => {
+    const cardInput = card.querySelector("input");
+    cardInput.checked = true;
+    cardInput.setAttribute("oninput", `replaceCoin(this,element)`); //maybe to remove this function and pass straight 'element'
+    card.className = "col-xl-5 mb-3";
+    card.childNodes[1].childNodes[3].childNodes[7].remove();
+  });
+}
+
+function replaceCoin(item,element) {
+  console.log(element);
+  const checkedCoin = item.parentElement.parentElement.parentElement.id;
+  const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
+  const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || [];
+  const findCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
+  console.log(findCoinFromLS);
+  const indexOfRemovedCoin = userCoins.findIndex(
+    (item) => item.id == checkedCoin
+  );
+  userCoins.splice(indexOfRemovedCoin, 1);
+  setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
+}
+
+function htmlTemplate(data) {
+  // const cards = document.querySelector(placement);
+  let html = "";
   for (let i = 0; i < data.length; i++) {
     html += `
-    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
+    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3" id="responsiveLayout">
       <div class="card" id="${data[i].id}">
         <h5 class="card-header">${data[i].symbol.toUpperCase()}</h5>
         <div class="card-body">
@@ -92,12 +121,17 @@ function drawData(data) {
         </div>
       </div>`;
   }
+  return html;
+}
+
+function drawData(data) {
+  const cards = document.querySelector(".row");
   if (data.length === 0) {
-    cards.innerHTML = 'Please enter valid name of coin';
+    cards.innerHTML = "Please enter valid name of coin";
   } else {
-    cards.innerHTML = html;
+    cards.innerHTML = htmlTemplate(data);
   }
-  const checkBoxes = document.querySelectorAll('#flexSwitchCheckDefault');
+  const checkBoxes = document.querySelectorAll("#flexSwitchCheckDefault");
   const favoriteCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
   if (favoriteCoins) {
     checkBoxes.forEach((box) => {
@@ -105,22 +139,25 @@ function drawData(data) {
       for (const value of iterator) {
         if (box.classList.contains(value.symbol)) {
           box.checked = true;
+          box.classList.remove("user-selected");
         }
       }
     });
   }
 
   checkBoxes.forEach((key) =>
-    key.addEventListener('click', function () {
+    key.addEventListener("click", function () {
       const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
       const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || [];
       const checkedCoin = this.parentElement.parentElement.parentElement.id;
       if (this.checked) {
-        this.classList.remove('user-selected');
+        if (userCoins <= 5) {
+          this.classList.remove("user-selected");
+        }
         const findCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
         setToLocalStorage([...userCoins, findCoinFromLS], SELECTED_COINS_LS_ID);
       } else {
-        this.classList.add('user-selected');
+        this.classList.add("user-selected");
         const indexOfRemovedCoin = userCoins.findIndex(
           (item) => item.id == checkedCoin
         );
@@ -143,7 +180,7 @@ async function getCurrencyPrice(obj, id) {
     );
   } else {
     if (document.querySelector('a[aria-expanded="false"]')) {
-      spinner.removeAttribute('hidden');
+      spinner.removeAttribute("hidden");
     }
     try {
       const response = await fetch(
@@ -161,7 +198,7 @@ async function getCurrencyPrice(obj, id) {
         setToSessionStorage([...coinPrice, priceObj], PRICE_OF_COINS);
       }
       showCurrencyPrice(priceObj, id);
-      spinner.setAttribute('hidden', '');
+      spinner.setAttribute("hidden", "");
     } catch (err) {
       console.log(err);
     }
@@ -170,7 +207,6 @@ async function getCurrencyPrice(obj, id) {
 
 function showCurrencyPrice(priceObj, id) {
   const coin = document.querySelector(`.price-info${id}`);
-  // add here (if) and check for session storage or else remove
   const html = `
   $: ${priceObj.usd}
   <br>
@@ -180,8 +216,6 @@ function showCurrencyPrice(priceObj, id) {
   `;
   coin.innerHTML = html;
 }
-
-setInterval(() => sessionStorage.clear(PRICE_OF_COINS), 120000);
 
 // Storage functions
 function setToLocalStorage(data, id) {
@@ -204,10 +238,12 @@ function getFromSessionStorage(id) {
   return JSON.parse(coins);
 }
 
-const input = document.querySelector('.form-control');
-input.addEventListener('input', () =>
+const input = document.querySelector(".form-control");
+input.addEventListener("input", () =>
   displaySearchCoins(input.value.toLowerCase())
 );
+
+setInterval(() => sessionStorage.clear(PRICE_OF_COINS), 120000);
 
 function displaySearchCoins(coins) {
   const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
