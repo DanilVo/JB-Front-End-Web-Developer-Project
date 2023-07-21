25194b27f01bf5ccfbd4b22c45bbc2cd6065f8ad
danil volobuyev <dani736@gmail.com>
2023-07-21 18:58:57 +0300

    finished required tasks for project

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,12 @@
-const LIST_OF_COINS_LS_ID = "coin";
-const SELECTED_COINS_LS_ID = "userCoins";
-const PRICE_OF_COINS = "coinsPrices";
+const LIST_OF_COINS_LS_ID = 'coin';
+const SELECTED_COINS_LS_ID = 'userCoins';
+const PRICE_OF_COINS = 'coinsPrices';
 const URL_API =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1";
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1';
 
-document.getElementById("home-page").addEventListener("click", getDataFromApi);
+document.getElementById('home-page').addEventListener('click', getDataFromApi);
 
-window.addEventListener("load", getDataFromApi());
+window.addEventListener('load', getDataFromApi);
 
 async function getDataFromApi() {
   try {
@@ -28,18 +28,17 @@ async function getDataFromApi() {
 function onToggleChange(element) {
   const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
   const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
-  const userCheckBoxes = document.querySelectorAll(".user-selected");
+  const userCheckBoxes = document.querySelectorAll('.user-selected');
   if (userCoins.length === 5) {
     userCheckBoxes.forEach((item) => {
-      item.setAttribute("data-bs-toggle", "modal");
-      item.setAttribute("data-bs-target", "#exampleModal");
+      item.setAttribute('data-bs-toggle', 'modal');
+      item.setAttribute('data-bs-target', '#exampleModal');
     });
   } else if (userCoins.length <= 4) {
     userCheckBoxes.forEach((item) => {
-      if (item.hasAttribute("data-bs-toggle")) {
-        console.log(userCoins.length);
-        item.removeAttribute("data-bs-toggle");
-        item.removeAttribute("data-bs-target");
+      if (item.hasAttribute('data-bs-toggle')) {
+        item.removeAttribute('data-bs-toggle');
+        item.removeAttribute('data-bs-target');
       }
     });
   }
@@ -47,51 +46,58 @@ function onToggleChange(element) {
     element.checked = false;
     userCoins.splice(userCoins.length - 1, 1);
     setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
-    const element1 = element.parentElement.parentElement.parentElement.id;
-    const findCoinFromLS = coinsList.find((item) => item.id === element1);
-    
-    clearCoin(findCoinFromLS);
+    const findCoinFromLS = coinsList.find(
+      (item) => item.id === element.parentElement.parentElement.parentElement.id
+    );
+    clearCoin(JSON.stringify(findCoinFromLS));
   }
 }
 
 function clearCoin(element) {
-    const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
-  const cards = document.querySelector("#selectedCoins");
+  const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
+  const cards = document.querySelector('#selectedCoins');
   cards.innerHTML = htmlTemplate(userCoins);
   placeCoinsInModal(element);
 }
 
 function placeCoinsInModal(element) {
-    console.log(element);
-    const cards = document.querySelectorAll(
-      "#selectedCoins > #responsiveLayout"
-    );
+  const cards = document.querySelectorAll('#selectedCoins > #responsiveLayout');
   cards.forEach((card) => {
-    const cardInput = card.querySelector("input");
+    const cardInput = card.querySelector('input');
     cardInput.checked = true;
-    cardInput.setAttribute("oninput", `replaceCoin(this,element)`); //maybe to remove this function and pass straight 'element'
-    card.className = "col-xl-5 mb-3";
+    cardInput.setAttribute('oninput', `replaceCoin(this,${element})`);
+    card.className = 'col-xl-5 mb-3';
     card.childNodes[1].childNodes[3].childNodes[7].remove();
+    cardInput.setAttribute('data-bs-dismiss', 'modal');
   });
 }
 
-function replaceCoin(item,element) {
-  console.log(element);
+function replaceCoin(item, element) {
   const checkedCoin = item.parentElement.parentElement.parentElement.id;
   const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
   const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || [];
   const findCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
-  console.log(findCoinFromLS);
   const indexOfRemovedCoin = userCoins.findIndex(
     (item) => item.id == checkedCoin
   );
-  userCoins.splice(indexOfRemovedCoin, 1);
+  userCoins.splice(indexOfRemovedCoin, 1, element);
+  const coinThatReplaced = document.querySelector(`.${findCoinFromLS.symbol}`);
+  coinThatReplaced.checked = false;
+  coinThatReplaced.classList.add('user-selected');
+  coinThatReplaced.setAttribute('data-bs-target', '#exampleModal');
+  coinThatReplaced.setAttribute('data-bs-toggle', 'modal');
+
+  const coinToReplace = document.querySelector(`.${element.symbol}`);
+  coinToReplace.checked = true;
+  coinToReplace.classList.remove('user-selected');
+  coinToReplace.removeAttribute('data-bs-target');
+  coinToReplace.removeAttribute('data-bs-toggle');
+
   setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
 }
 
 function htmlTemplate(data) {
-  // const cards = document.querySelector(placement);
-  let html = "";
+  let html = '';
   for (let i = 0; i < data.length; i++) {
     html += `
     <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3" id="responsiveLayout">
@@ -125,13 +131,13 @@ function htmlTemplate(data) {
 }
 
 function drawData(data) {
-  const cards = document.querySelector(".row");
+  const cards = document.querySelector('.row');
   if (data.length === 0) {
-    cards.innerHTML = "Please enter valid name of coin";
+    cards.innerHTML = 'Please enter valid name of coin';
   } else {
     cards.innerHTML = htmlTemplate(data);
   }
-  const checkBoxes = document.querySelectorAll("#flexSwitchCheckDefault");
+  const checkBoxes = document.querySelectorAll('#flexSwitchCheckDefault');
   const favoriteCoins = getFromLocalStorage(SELECTED_COINS_LS_ID);
   if (favoriteCoins) {
     checkBoxes.forEach((box) => {
@@ -139,27 +145,28 @@ function drawData(data) {
       for (const value of iterator) {
         if (box.classList.contains(value.symbol)) {
           box.checked = true;
-          box.classList.remove("user-selected");
+          box.classList.remove('user-selected');
         }
       }
     });
   }
+  triggerModalOnFiveItems();
 
   checkBoxes.forEach((key) =>
-    key.addEventListener("click", function () {
+    key.addEventListener('click', function () {
       const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
       const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || [];
       const checkedCoin = this.parentElement.parentElement.parentElement.id;
       if (this.checked) {
-        if (userCoins <= 5) {
-          this.classList.remove("user-selected");
+        if (userCoins.length < 5) {
+          this.classList.remove('user-selected');
         }
         const findCoinFromLS = coinsList.find((item) => item.id == checkedCoin);
         setToLocalStorage([...userCoins, findCoinFromLS], SELECTED_COINS_LS_ID);
       } else {
-        this.classList.add("user-selected");
+        this.classList.add('user-selected');
         const indexOfRemovedCoin = userCoins.findIndex(
-          (item) => item.id == checkedCoin
+          (item) => item.id === checkedCoin
         );
         userCoins.splice(indexOfRemovedCoin, 1);
         setToLocalStorage(userCoins, SELECTED_COINS_LS_ID);
@@ -168,37 +175,59 @@ function drawData(data) {
   );
 }
 
+function triggerModalOnFiveItems() {
+  const userCoins = getFromLocalStorage(SELECTED_COINS_LS_ID) || [];
+  const userCheckBoxes = document.querySelectorAll('.user-selected');
+  if (userCoins.length === 5) {
+    userCheckBoxes.forEach((item) => {
+      item.setAttribute('data-bs-toggle', 'modal');
+      item.setAttribute('data-bs-target', '#exampleModal');
+    });
+  }
+}
+
 async function getCurrencyPrice(obj, id) {
   const spinner = document.querySelector(`#spinner${id}`);
   const coinPrice = getFromSessionStorage(PRICE_OF_COINS) || [];
   const coinId = obj.parentElement.parentElement.id;
-  console.log(coinId);
-  if (coinPrice.find((coin) => coin.name.toLowerCase() === coinId)) {
+  let dateNow = new Date().getTime();
+  if (
+    coinPrice.find(
+      (coin) =>
+        coin.name.toLowerCase() === coinId && dateNow - coin.timeAssigned < 5000
+    )
+  ) {
     showCurrencyPrice(
       coinPrice.find((coin) => coin.name.toLowerCase() === coinId),
       id
     );
   } else {
     if (document.querySelector('a[aria-expanded="false"]')) {
-      spinner.removeAttribute("hidden");
+      spinner.removeAttribute('hidden');
     }
     try {
       const response = await fetch(
         `https://api.coingecko.com/api/v3/coins/${coinId}`
       );
       const data = await response.json();
-      let priceObj = {
+      const priceObj = {
         name: data.id,
         usd: data.market_data.current_price.usd,
         euro: data.market_data.current_price.eur,
         ils: data.market_data.current_price.ils,
+        timeAssigned: new Date().getTime(),
       };
-      console.log(priceObj);
       if (!coinPrice.some((coin) => coin.name === priceObj.name)) {
+        setToSessionStorage([...coinPrice, priceObj], PRICE_OF_COINS);
+      } else {
+        const indexOfPreviousData = coinPrice.findIndex(
+          (item) => coinId === item.name
+        );
+        coinPrice.splice(indexOfPreviousData, 1);
         setToSessionStorage([...coinPrice, priceObj], PRICE_OF_COINS);
       }
       showCurrencyPrice(priceObj, id);
-      spinner.setAttribute("hidden", "");
+      spinner.setAttribute('hidden', '');
     } catch (err) {
       console.log(err);
     }
@@ -238,12 +267,10 @@ function getFromSessionStorage(id) {
   return JSON.parse(coins);
 }
 
-const input = document.querySelector(".form-control");
-input.addEventListener("input", () =>
+const input = document.querySelector('.form-control');
+input.addEventListener('input', () =>
   displaySearchCoins(input.value.toLowerCase())
 );
-
-setInterval(() => sessionStorage.clear(PRICE_OF_COINS), 120000);
 
 function displaySearchCoins(coins) {
   const coinsList = getFromLocalStorage(LIST_OF_COINS_LS_ID);
