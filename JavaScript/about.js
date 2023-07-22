037a57d9c26c989cce1ca1e95c088fe34463f110
danil volobuyev <dani736@gmail.com>
2023-07-23 00:10:06 +0300

    test

--- JavaScript/about.js
+++ JavaScript/about.js
@@ -2,6 +2,14 @@ const about = document.querySelector("#about")
 about.addEventListener('click', aboutPage)
 
 function aboutPage() {
-    const container = document.querySelector('.row')
-    container.innerHTML = "about";
+    // const container = document.querySelector('.row')
+    // container.innerHTML = "about";
+    console.log($('#about'))
 }
+
+// $(()=> {
+//     $('#about').click(()=>{
+
+//         ('.row').text('HELLO')
+//     })
+// })
